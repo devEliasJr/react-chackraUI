f0a69d814aa6372c5bc6269f2f6b55c91cfce601
devEliasJr <eliasengmecanica@gmail.com>
2023-08-02 01:10:01 -0300

    refactor: Refactor router component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import "./App.css";
 import { DefaultLayout } from "./Layout/DefaultLayout";
 import ErrorPage from "./routes/ErrorPage";
 import { Contact } from "./pages/Contact";
 import ContactDetails from "./pages/Contact/ContacDetails";
 import Dashboard from "./pages/DashBoard";
-import Create from "./pages/Create";
+import Create, { createAction } from "./pages/Create";
 import Profile from "./pages/Profile";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <DefaultLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/login", element: <Login /> },
-      { path: "/dashboard", element: <Dashboard />},
-      { path: "/create", element: <Create /> },
-      { path: "/profile", element: <Profile /> },
-      { path: "/contact", element: <Contact /> },
-      { path: "/contact/:id", element: <ContactDetails /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<DefaultLayout />}>
+      <Route index element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/create" element={<Create />} action={createAction} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/contact/:id" element={<ContactDetails />} />
+    </Route>
+  )
+);
 
 export default function App() {
   return (
